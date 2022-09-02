963b5275a88be8deaec8c54285699186da1a18f7
SheYu09 <805608215@qq.com>
2022-09-02 12:36:06 +0800

    *

--- promote.py
+++ promote.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*
 '''
-项目名称: JD-Script / jd_promote
-活动名称: new Env('平行时空')
+项目名称: JD-Script / promote
+活动名称: 平行时空
 Author: SheYu09
 cron: 7 9 * * * promote.py
+new Env('平行时空')
 '''
 try:
 	from jd_promote import start
